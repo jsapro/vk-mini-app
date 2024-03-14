606a2e2018e3741fbd5def1fd490735867ae2e95
jsapro <77.3.77@mail.ru>
2024-03-14 20:14:02 +0300

    feature: add Home page

--- src/panels/Home.tsx
+++ src/panels/Home.tsx
@@ -1,44 +1,67 @@
 import { FC } from 'react';
 import {
-  Panel,
-  PanelHeader,
-  Header,
-  Button,
-  Group,
-  Cell,
-  Div,
-  Avatar,
-  NavIdProps,
+	Panel,
+	PanelHeader,
+	Header,
+	Button,
+	Group,
+	Cell,
+	Div,
+	Avatar,
+	NavIdProps,
+	Spacing,
 } from '@vkontakte/vkui';
 import { UserInfo } from '@vkontakte/vk-bridge';
 import { useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
 
 export interface HomeProps extends NavIdProps {
-  fetchedUser?: UserInfo;
+	fetchedUser?: UserInfo;
 }
 
 export const Home: FC<HomeProps> = ({ id, fetchedUser }) => {
-  const { photo_200, city, first_name, last_name } = { ...fetchedUser };
-  const routeNavigator = useRouteNavigator();
+	const { photo_200, city, first_name, last_name } = { ...fetchedUser };
+	const routeNavigator = useRouteNavigator();
 
-  return (
-    <Panel id={id}>
-      <PanelHeader>Главная</PanelHeader>
-      {fetchedUser && (
-        <Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
-          <Cell before={photo_200 && <Avatar src={photo_200} />} subtitle={city?.title}>
-            {`${first_name} ${last_name}`}
-          </Cell>
-        </Group>
-      )}
+	return (
+		<Panel id={id}>
+			<PanelHeader>Главная</PanelHeader>
+			{fetchedUser && (
+				<Group header={<Header mode="secondary">Привет!!! 👋</Header>}>
+					<Cell
+						before={photo_200 && <Avatar src={photo_200} />}
+						subtitle={city?.title}>
+						{`${first_name} ${last_name}`}
+					</Cell>
+				</Group>
+			)}
 
-      <Group header={<Header mode="secondary">Navigation Example</Header>}>
-        <Div>
-          <Button stretched size="l" mode="secondary" onClick={() => routeNavigator.push('persik')}>
-            Покажите Персика, пожалуйста!
-          </Button>
-        </Div>
-      </Group>
-    </Panel>
-  );
+			<Group header={<Header mode="secondary"> Выбирай  ❤</Header>}>
+				<Div>
+					<Button
+						stretched
+						size="l"
+						mode="primary"
+						onClick={() => routeNavigator.push('robots')}>
+						{last_name}, здесь Futurama-style robots!
+					</Button>
+					<Spacing size={10} />
+					<Button
+						stretched
+						size="l"
+						mode="primary"
+						onClick={() => routeNavigator.push('cats')}>
+						А здесь факты о 🐈 🐈 🐈!
+					</Button>
+					<Spacing size={10} />
+					<Button
+						stretched
+						size="l"
+						mode="primary"
+						onClick={() => routeNavigator.push('age')}>
+						{last_name}, попробуй определить возраст по имени!
+					</Button>
+				</Div>
+			</Group>
+		</Panel>
+	);
 };
